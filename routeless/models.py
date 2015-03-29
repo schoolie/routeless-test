c63e1be0fadd3351d68eb4297aad1c8c98947f54
schoolie <brian.p.schoolcraft@gmail.com>
2015-03-28 21:50:02 -0400

    working on tests

--- routeless/models.py
+++ routeless/models.py
@@ -6,18 +6,17 @@ from routeless.extensions import db, admin
 
 class User(db.Model):
     __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     events = db.relationship('Event', backref='user', lazy='dynamic')
     courses = db.relationship('Course', backref='creator', lazy='dynamic')
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
-    password = db.Column(db.String(128))
     password_hash = db.Column(db.String(128))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
-        self.hash_password(self.password)
-    
+
     def hash_password(self, password):
         self.password_hash = generate_password_hash(password)
         
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -13,7 +13,6 @@ class BasicsTestCase(unittest.TestCase):
 
         self.client = self.app.test_client(use_cookies=True)
 
-
     def tearDown(self):
         db.session.remove()
         db.drop_all()
@@ -25,6 +24,7 @@ class BasicsTestCase(unittest.TestCase):
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
 
+    '''
     def test_create_user(self):
         data = {'username': 'Brian',
                 'email': 'brian.p.schoolcraft@gmail.com',
@@ -36,16 +36,40 @@ class BasicsTestCase(unittest.TestCase):
                                 data=json_data,
                                 headers={'content-type':'application/json'})
 
+        print response.data
         json_data = json.loads(response.data)
-        print json_data['password_hash']
+        # print json_data['password_hash']
         self.assertEqual(response.status_code, 201)
         self.assertEqual(json_data['password'], 'Amanda09')
+    '''
+
+
+
+class APITestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+        self.client = self.app.test_client(use_cookies=True)
+
+        user = User(username='Brian',
+                    email='brian.p.schoolcraft@gmail.com')
+
+        user.hash_password('Amanda09')
+
+        db.session.add(user)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
     def test_create_course(self):
-
-        u = User(username='Brian', email='brian@example.com', password='cat')
-        db.session.add(u)
-        db.session.commit()
+        u = User.query.first()
 
         data = {'centerlat': 45.23,
                 'centerlon': 54.12,
@@ -70,3 +94,15 @@ class BasicsTestCase(unittest.TestCase):
                         headers={'content-type':'application/json'})
         # print response.data
 
+    def test_get_user(self):
+        response = self.client.get('/api/user',
+                                    headers={'content-type':'application/json'})
+        json_data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(json_data['objects']), 1)
+        self.assertEqual(json_data['num_results'], 1)
+
+        json_user = json_data['objects'][0]
+
+        self.assertEqual(json_user['email'], 'brian.p.schoolcraft@gmail.com')
